280f28fc73b9543cd285fa1ffc139aefda287804
Chris Klimas <chris@twofoldsecret.com>
2013-04-22 23:07:13 -0400

    Remove Underscore templating, partial progress on macros

--- js/harlowe.js
+++ js/harlowe.js
@@ -1,12 +1,11 @@
 require.config({
 	shim:
 	{
-		'underscore': { exports: '_' },
 		'showdown': { exports: 'Showdown' }
 	}
 });
 
-define(['jquery', 'underscore', 'showdown'], function ($, _, Showdown)
+define(['jquery', 'showdown', 'macros'], function ($, Showdown)
 {
 	var markdown = new Showdown.converter();
 
@@ -23,10 +22,9 @@ define(['jquery', 'underscore', 'showdown'], function ($, _, Showdown)
 
 	function render (source)
 	{
-		// jQuery will escape out the underscore leaders
+		// basic Markdown
 
-		var template = _.template(source.replace(/&lt;%/g, '<%').replace(/%&gt;/g, '%>'));
-		var html = markdown.makeHtml(template());
+		var html = markdown.makeHtml(source);
 
 		// replace [[ ]] with twine links
 
@@ -45,6 +43,60 @@ define(['jquery', 'underscore', 'showdown'], function ($, _, Showdown)
 			return '<a href="#' + _.escape(p1.replace(/\s/g, '')) + '" data-twinelink="' + p1 + '">' + p1 + '</a>';
 		});
 
+		// convert macro invocations to spans
+
+		var macros = [];
+
+		// <<macro>>inner content<</macro>> format
+
+		html = html.replace(/&lt;&lt;(\w+).*?&lt;&lt;\/\1&gt;&gt;/g, function (match, p1)
+		{
+			macros.push([p1, match]);
+			return '<span data-macro="' + macros.length + '"></span>';
+		});
+
+		// <<macro>>inner content<<endmacro>> format
+
+		html = html.replace(/&lt;&lt;(\w+).*?&lt;&lt;end\1&gt;&gt;/g, function (match, p1)
+		{
+			macros.push([p1, match]);
+			return '<span data-macro="' + macros.length + '"></span>';
+		});
+
+		// <<macro>> without closing <</macro>>
+
+		html = html.replace(/&lt;&lt;(\w+).*?&gt;&gt;/g, function (match, p1)
+		{
+			macros.push([p1, match]);
+			return '<span data-macro="' + macros.length + '"></span>';
+		});
+
+		// convert into real element, then render macros into their spans
+
+		html = $(html);
+
+		for (var i = 0; i < macros.length; i++)
+		{
+			var span = html.find('span[data-macro="' + i + '"]');
+
+			if (window.story.macros[macros[i][0]])
+			{
+				// tokenize arguments
+				// e.g. 1 "two three" 'four five' "six \" seven" 'eight \' nine'
+				// becomes [1, "two three", "four five", 'six " seven', "eight ' nine"]
+
+				var rawCall = macros[i][1].replace(/&lt;/g, '<').replace(/&gt;/g, '>');
+
+				console.log('rawCall', rawCall);
+
+				var rawArgs = rawCall.replace(/^.*?\s/, '').replace(/>>.*/, '');
+
+				console.log('rawArgs', rawArgs);
+			}
+			else
+				span.html('No macro named ' + macros[i][0]);
+		};
+
 		return html;
 	};
 
@@ -56,9 +108,10 @@ define(['jquery', 'underscore', 'showdown'], function ($, _, Showdown)
 			throw new Error("No passage exists with id " + id);
 
 		el = el || $('#story');	
-		el.append('<section><a class="permalink" href="#' + passage.attr('data-name') + '"' +
-				  ' title="Permanent link to this passage">&para;</a>' +
-				  render(passage.html()) + '</section>');
+		var container = $('<section><a class="permalink" href="#' + passage.attr('data-name') + '"' +
+				  ' title="Permanent link to this passage">&para;</a></section>');
+		container.append(render(passage.html()));
+		el.append(container);
 	};
 
 	function showName (name, el)
@@ -69,8 +122,9 @@ define(['jquery', 'underscore', 'showdown'], function ($, _, Showdown)
 			throw new Error("No passage exists with name " + name);
 
 		el = el || $('#story');	
-		el.append('<section><a class="permalink" href="#' + passage.attr('data-name') + '"' +
-				  ' title="Permanent link to this passage">&para;</a>' +
-				  render(passage.html()) + '</section>');
+		var container = $('<section><a class="permalink" href="#' + passage.attr('data-name') + '"' +
+				  ' title="Permanent link to this passage">&para;</a></section>');
+		container.append(render(passage.html()));
+		el.append(container);
 	};
 });
---
+++ js/macros.js
@@ -0,0 +1,7 @@
+window.story = window.story || {};
+window.story.macros = window.story.macros || {};
+
+window.story.macros.test = function (arg1, arg2, arg3)
+{
+	return 'arg1 is ' + arg1 + ', arg2 is ' + arg2 + ', arg3 is ' + arg3;
+};
