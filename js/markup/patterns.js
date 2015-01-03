de48e12cdf74d91837a8ba795bcef985e9da07d3
Leon Arnott <L_1_L_0@yahoo.com>
2015-01-03 21:33:26 +1000

    Added some test specs for the link syntax.
    This currently doesn't test the legacy link syntax [[text|passage]], but this may be added in the future.

--- js/markup/patterns.js
+++ js/markup/patterns.js
@@ -182,7 +182,7 @@
 			leftSeparator:     "<\\-",
 			closer:            "\\]\\]",
 			legacySeparator:   "\\|",
-			legacyText:        "(" + notChars("]|") + "?)",
+			legacyText:        "(" + either("[^\\|\\]]", "\\]" + notBefore("\\]")) + "+)",
 		},
 		
 		/*
